7b76dfdfedf9d9d37c5af270441e6333ac5fd2c2
Craig Sloggett <craigsloggett@gmail.com>
2024-03-18 10:57:38 -0400

    Add fields as outputs in the example

--- examples/provider-install-verification/outputs.tf
+++ examples/provider-install-verification/outputs.tf
@@ -13,6 +13,11 @@ output "github_repository_example_id" {
   value       = data.github_repository.example.id
 }
 
+output "github_repository_example_node_id" {
+  description = "This is an example."
+  value       = data.github_repository.example.node_id
+}
+
 output "github_repository_example_name" {
   description = "This is an example."
   value       = data.github_repository.example.name
@@ -21,4 +26,79 @@ output "github_repository_example_name" {
 output "github_repository_example_full_name" {
   description = "This is an example."
   value       = data.github_repository.example.full_name
+}
+
+output "github_repository_example_description" {
+  description = "This is an example."
+  value       = data.github_repository.example.description
+}
+
+output "github_repository_example_homepage" {
+  description = "This is an example."
+  value       = data.github_repository.example.homepage
+}
+
+output "github_repository_example_default_branch" {
+  description = "This is an example."
+  value       = data.github_repository.example.default_branch
+}
+
+output "github_repository_example_master_branch" {
+  description = "This is an example."
+  value       = data.github_repository.example.master_branch
+}
+
+output "github_repository_example_html_url" {
+  description = "This is an example."
+  value       = data.github_repository.example.html_url
+}
+
+output "github_repository_example_clone_url" {
+  description = "This is an example."
+  value       = data.github_repository.example.clone_url
+}
+
+output "github_repository_example_git_url" {
+  description = "This is an example."
+  value       = data.github_repository.example.git_url
+}
+
+output "github_repository_example_mirror_url" {
+  description = "This is an example."
+  value       = data.github_repository.example.mirror_url
+}
+
+output "github_repository_example_ssh_url" {
+  description = "This is an example."
+  value       = data.github_repository.example.ssh_url
+}
+
+output "github_repository_example_svn_url" {
+  description = "This is an example."
+  value       = data.github_repository.example.svn_url
+}
+
+output "github_repository_example_language" {
+  description = "This is an example."
+  value       = data.github_repository.example.language
+}
+
+output "github_repository_example_squash_merge_commit_title" {
+  description = "This is an example."
+  value       = data.github_repository.example.squash_merge_commit_title
+}
+
+output "github_repository_example_squash_merge_commit_message" {
+  description = "This is an example."
+  value       = data.github_repository.example.squash_merge_commit_message
+}
+
+output "github_repository_example_merge_commit_title" {
+  description = "This is an example."
+  value       = data.github_repository.example.merge_commit_title
+}
+
+output "github_repository_example_merge_commit_message" {
+  description = "This is an example."
+  value       = data.github_repository.example.merge_commit_message
 }
