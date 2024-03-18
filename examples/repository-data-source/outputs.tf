fa5047c213020cb9ea75ce410e528839ce100d84
Craig Sloggett <craigsloggett@gmail.com>
2024-03-18 14:22:43 -0400

    Add fields with simple base types to the repository datasource (#8)
    * Update dependencies
    
    * Add all string fields from the Repository struct
    
    * Add fields as outputs in the example
    
    * Rename example directory to match functionality
    
    * Add all boolean fields
    
    * Add all integer fields

---
+++ examples/repository-data-source/outputs.tf
@@ -0,0 +1,200 @@
+output "github_repository_example_owner" {
+  description = "This is an example."
+  value       = data.github_repository.example.owner
+}
+
+output "github_repository_example_repo" {
+  description = "This is an example."
+  value       = data.github_repository.example.repo
+}
+
+output "github_repository_example_id" {
+  description = "This is an example."
+  value       = data.github_repository.example.id
+}
+
+output "github_repository_example_node_id" {
+  description = "This is an example."
+  value       = data.github_repository.example.node_id
+}
+
+output "github_repository_example_name" {
+  description = "This is an example."
+  value       = data.github_repository.example.name
+}
+
+output "github_repository_example_full_name" {
+  description = "This is an example."
+  value       = data.github_repository.example.full_name
+}
+
+output "github_repository_example_description" {
+  description = "This is an example."
+  value       = data.github_repository.example.description
+}
+
+output "github_repository_example_homepage" {
+  description = "This is an example."
+  value       = data.github_repository.example.homepage
+}
+
+output "github_repository_example_default_branch" {
+  description = "This is an example."
+  value       = data.github_repository.example.default_branch
+}
+
+output "github_repository_example_master_branch" {
+  description = "This is an example."
+  value       = data.github_repository.example.master_branch
+}
+
+output "github_repository_example_html_url" {
+  description = "This is an example."
+  value       = data.github_repository.example.html_url
+}
+
+output "github_repository_example_clone_url" {
+  description = "This is an example."
+  value       = data.github_repository.example.clone_url
+}
+
+output "github_repository_example_git_url" {
+  description = "This is an example."
+  value       = data.github_repository.example.git_url
+}
+
+output "github_repository_example_mirror_url" {
+  description = "This is an example."
+  value       = data.github_repository.example.mirror_url
+}
+
+output "github_repository_example_ssh_url" {
+  description = "This is an example."
+  value       = data.github_repository.example.ssh_url
+}
+
+output "github_repository_example_svn_url" {
+  description = "This is an example."
+  value       = data.github_repository.example.svn_url
+}
+
+output "github_repository_example_language" {
+  description = "This is an example."
+  value       = data.github_repository.example.language
+}
+
+output "github_repository_example_fork" {
+  description = "This is an example."
+  value       = data.github_repository.example.fork
+}
+
+output "github_repository_example_forks_count" {
+  description = "The number of forks of the repository."
+  value       = data.github_repository.example.forks_count
+}
+
+output "github_repository_example_network_count" {
+  description = "The count of the repository network."
+  value       = data.github_repository.example.network_count
+}
+
+output "github_repository_example_open_issues_count" {
+  description = "The number of open issues in the repository."
+  value       = data.github_repository.example.open_issues_count
+}
+
+output "github_repository_example_stargazers_count" {
+  description = "The number of users who have starred the repository."
+  value       = data.github_repository.example.stargazers_count
+}
+
+output "github_repository_example_subscribers_count" {
+  description = "The number of users subscribed to the repository."
+  value       = data.github_repository.example.subscribers_count
+}
+
+output "github_repository_example_size" {
+  description = "The size of the repository, in kilobytes."
+  value       = data.github_repository.example.size
+}
+
+output "github_repository_example_auto_init" {
+  description = "This is an example."
+  value       = data.github_repository.example.auto_init
+}
+
+output "github_repository_example_allow_rebase_merge" {
+  description = "This is an example."
+  value       = data.github_repository.example.allow_rebase_merge
+}
+
+output "github_repository_example_allow_update_branch" {
+  description = "This is an example."
+  value       = data.github_repository.example.allow_update_branch
+}
+
+output "github_repository_example_allow_squash_merge" {
+  description = "This is an example."
+  value       = data.github_repository.example.allow_squash_merge
+}
+
+output "github_repository_example_allow_merge_commit" {
+  description = "This is an example."
+  value       = data.github_repository.example.allow_merge_commit
+}
+
+output "github_repository_example_allow_auto_merge" {
+  description = "This is an example."
+  value       = data.github_repository.example.allow_auto_merge
+}
+
+output "github_repository_example_allow_forking" {
+  description = "This is an example."
+  value       = data.github_repository.example.allow_forking
+}
+
+output "github_repository_example_web_commit_signoff_required" {
+  description = "This is an example."
+  value       = data.github_repository.example.web_commit_signoff_required
+}
+
+output "github_repository_example_delete_branch_on_merge" {
+  description = "This is an example."
+  value       = data.github_repository.example.delete_branch_on_merge
+}
+
+output "github_repository_example_use_squash_pr_title_as_default" {
+  description = "This is an example."
+  value       = data.github_repository.example.use_squash_pr_title_as_default
+}
+
+output "github_repository_example_squash_merge_commit_title" {
+  description = "This is an example."
+  value       = data.github_repository.example.squash_merge_commit_title
+}
+
+output "github_repository_example_squash_merge_commit_message" {
+  description = "This is an example."
+  value       = data.github_repository.example.squash_merge_commit_message
+}
+
+output "github_repository_example_merge_commit_title" {
+  description = "This is an example."
+  value       = data.github_repository.example.merge_commit_title
+}
+
+output "github_repository_example_merge_commit_message" {
+  description = "This is an example."
+  value       = data.github_repository.example.merge_commit_message
+}
+
+output "github_repository_example_archived" {
+  description = "This is an example."
+  value       = data.github_repository.example.archived
+}
+
+output "github_repository_example_disabled" {
+  description = "This is an example."
+  value       = data.github_repository.example.disabled
+}
+
--- internal/resources/repositories/data_source_github_repository.go
+++ internal/resources/repositories/data_source_github_repository.go
@@ -17,11 +17,46 @@ type GitHubRepository struct {
 }
 
 type GitHubRepositoryModel struct {
-	Owner    types.String `tfsdk:"owner"`
-	Repo     types.String `tfsdk:"repo"`
-	Id       types.Int64  `tfsdk:"id"`
-	Name     types.String `tfsdk:"name"`
-	FullName types.String `tfsdk:"full_name"`
+	Owner                     types.String `tfsdk:"owner"`
+	Repo                      types.String `tfsdk:"repo"`
+	Id                        types.Int64  `tfsdk:"id"`
+	NodeID                    types.String `tfsdk:"node_id"`
+	Name                      types.String `tfsdk:"name"`
+	FullName                  types.String `tfsdk:"full_name"`
+	Description               types.String `tfsdk:"description"`
+	Homepage                  types.String `tfsdk:"homepage"`
+	DefaultBranch             types.String `tfsdk:"default_branch"`
+	MasterBranch              types.String `tfsdk:"master_branch"`
+	HTMLURL                   types.String `tfsdk:"html_url"`
+	CloneURL                  types.String `tfsdk:"clone_url"`
+	GitURL                    types.String `tfsdk:"git_url"`
+	MirrorURL                 types.String `tfsdk:"mirror_url"`
+	SSHURL                    types.String `tfsdk:"ssh_url"`
+	SVNURL                    types.String `tfsdk:"svn_url"`
+	Language                  types.String `tfsdk:"language"`
+	Fork                      types.Bool   `tfsdk:"fork"`
+	ForksCount                types.Int64  `tfsdk:"forks_count"`
+	NetworkCount              types.Int64  `tfsdk:"network_count"`
+	OpenIssuesCount           types.Int64  `tfsdk:"open_issues_count"`
+	StargazersCount           types.Int64  `tfsdk:"stargazers_count"`
+	SubscribersCount          types.Int64  `tfsdk:"subscribers_count"`
+	Size                      types.Int64  `tfsdk:"size"`
+	AutoInit                  types.Bool   `tfsdk:"auto_init"`
+	AllowRebaseMerge          types.Bool   `tfsdk:"allow_rebase_merge"`
+	AllowUpdateBranch         types.Bool   `tfsdk:"allow_update_branch"`
+	AllowSquashMerge          types.Bool   `tfsdk:"allow_squash_merge"`
+	AllowMergeCommit          types.Bool   `tfsdk:"allow_merge_commit"`
+	AllowAutoMerge            types.Bool   `tfsdk:"allow_auto_merge"`
+	AllowForking              types.Bool   `tfsdk:"allow_forking"`
+	WebCommitSignoffRequired  types.Bool   `tfsdk:"web_commit_signoff_required"`
+	DeleteBranchOnMerge       types.Bool   `tfsdk:"delete_branch_on_merge"`
+	UseSquashPRTitleAsDefault types.Bool   `tfsdk:"use_squash_pr_title_as_default"`
+	SquashMergeCommitTitle    types.String `tfsdk:"squash_merge_commit_title"`
+	SquashMergeCommitMessage  types.String `tfsdk:"squash_merge_commit_message"`
+	MergeCommitTitle          types.String `tfsdk:"merge_commit_title"`
+	MergeCommitMessage        types.String `tfsdk:"merge_commit_message"`
+	Archived                  types.Bool   `tfsdk:"archived"`
+	Disabled                  types.Bool   `tfsdk:"disabled"`
 }
 
 func NewGitHubRepository() datasource.DataSource {
@@ -50,6 +85,11 @@ func (d *GitHubRepository) Schema(_ context.Context, _ datasource.SchemaRequest,
 				MarkdownDescription: "GitHub ID for the repository.",
 				Computed:            true,
 			},
+			"node_id": schema.StringAttribute{
+				Description:         "The node ID of the repository.",
+				MarkdownDescription: "The node ID of the repository.",
+				Computed:            true,
+			},
 			"name": schema.StringAttribute{
 				Description:         "The name of the repository.",
 				MarkdownDescription: "The name of the repository.",
@@ -60,6 +100,176 @@ func (d *GitHubRepository) Schema(_ context.Context, _ datasource.SchemaRequest,
 				MarkdownDescription: "The full name of the repository.",
 				Computed:            true,
 			},
+			"description": schema.StringAttribute{
+				Description:         "The description of the repository.",
+				MarkdownDescription: "The description of the repository.",
+				Computed:            true,
+			},
+			"homepage": schema.StringAttribute{
+				Description:         "The homepage of the repository.",
+				MarkdownDescription: "The homepage of the repository.",
+				Computed:            true,
+			},
+			"default_branch": schema.StringAttribute{
+				Description:         "The repository's default branch.",
+				MarkdownDescription: "The repository's default branch.",
+				Computed:            true,
+			},
+			"master_branch": schema.StringAttribute{
+				Description:         "The repository's master branch.",
+				MarkdownDescription: "The repository's master branch.",
+				Computed:            true,
+			},
+			"html_url": schema.StringAttribute{
+				Description:         "The HTML URL of the repository.",
+				MarkdownDescription: "The HTML URL of the repository.",
+				Computed:            true,
+			},
+			"clone_url": schema.StringAttribute{
+				Description:         "The URL used for cloning the repository.",
+				MarkdownDescription: "The URL used for cloning the repository.",
+				Computed:            true,
+			},
+			"git_url": schema.StringAttribute{
+				Description:         "The git URL of the repository.",
+				MarkdownDescription: "The git URL of the repository.",
+				Computed:            true,
+			},
+			"mirror_url": schema.StringAttribute{
+				Description:         "The mirror URL of the repository.",
+				MarkdownDescription: "The mirror URL of the repository.",
+				Computed:            true,
+			},
+			"ssh_url": schema.StringAttribute{
+				Description:         "The SSH URL of the repository.",
+				MarkdownDescription: "The SSH URL of the repository.",
+				Computed:            true,
+			},
+			"svn_url": schema.StringAttribute{
+				Description:         "The SVN URL of the repository.",
+				MarkdownDescription: "The SVN URL of the repository.",
+				Computed:            true,
+			},
+			"language": schema.StringAttribute{
+				Description:         "The primary language of the repository.",
+				MarkdownDescription: "The primary language of the repository.",
+				Computed:            true,
+			},
+			"fork": schema.BoolAttribute{
+				Description:         "Indicates if the repository is a fork.",
+				MarkdownDescription: "Indicates if the repository is a fork.",
+				Computed:            true,
+			},
+			"forks_count": schema.Int64Attribute{
+				Description:         "The number of forks of the repository.",
+				MarkdownDescription: "The number of forks of the repository.",
+				Computed:            true,
+			},
+			"network_count": schema.Int64Attribute{
+				Description:         "The number of repositories in the network.",
+				MarkdownDescription: "The number of repositories in the network.",
+				Computed:            true,
+			},
+			"open_issues_count": schema.Int64Attribute{
+				Description:         "The number of open issues in the repository.",
+				MarkdownDescription: "The number of open issues in the repository.",
+				Computed:            true,
+			},
+			"stargazers_count": schema.Int64Attribute{
+				Description:         "The number of users who have starred the repository.",
+				MarkdownDescription: "The number of users who have starred the repository.",
+				Computed:            true,
+			},
+			"subscribers_count": schema.Int64Attribute{
+				Description:         "The number of users subscribed to the repository.",
+				MarkdownDescription: "The number of users subscribed to the repository.",
+				Computed:            true,
+			},
+			"size": schema.Int64Attribute{
+				Description:         "The size of the repository, in kilobytes.",
+				MarkdownDescription: "The size of the repository, in kilobytes.",
+				Computed:            true,
+			},
+			"auto_init": schema.BoolAttribute{
+				Description:         "Indicates if the repository is initialized with a README.",
+				MarkdownDescription: "Indicates if the repository is initialized with a README.",
+				Computed:            true,
+			},
+			"allow_rebase_merge": schema.BoolAttribute{
+				Description:         "Indicates if rebase merging is allowed in the repository.",
+				MarkdownDescription: "Indicates if rebase merging is allowed in the repository.",
+				Computed:            true,
+			},
+			"allow_update_branch": schema.BoolAttribute{
+				Description:         "Indicates if updating a pull request head branch that is behind its base branch is allowed.",
+				MarkdownDescription: "Indicates if updating a pull request head branch that is behind its base branch is allowed.",
+				Computed:            true,
+			},
+			"allow_squash_merge": schema.BoolAttribute{
+				Description:         "Indicates if squash merging is allowed in the repository.",
+				MarkdownDescription: "Indicates if squash merging is allowed in the repository.",
+				Computed:            true,
+			},
+			"allow_merge_commit": schema.BoolAttribute{
+				Description:         "Indicates if merge commits are allowed in the repository.",
+				MarkdownDescription: "Indicates if merge commits are allowed in the repository.",
+				Computed:            true,
+			},
+			"allow_auto_merge": schema.BoolAttribute{
+				Description:         "Indicates if auto-merging is allowed in the repository.",
+				MarkdownDescription: "Indicates if auto-merging is allowed in the repository.",
+				Computed:            true,
+			},
+			"allow_forking": schema.BoolAttribute{
+				Description:         "Indicates if forking is allowed for the repository.",
+				MarkdownDescription: "Indicates if forking is allowed for the repository.",
+				Computed:            true,
+			},
+			"web_commit_signoff_required": schema.BoolAttribute{
+				Description:         "Indicates if commit signoff is required for web-based commits.",
+				MarkdownDescription: "Indicates if commit signoff is required for web-based commits.",
+				Computed:            true,
+			},
+			"delete_branch_on_merge": schema.BoolAttribute{
+				Description:         "Indicates if branches are automatically deleted when pull requests are merged.",
+				MarkdownDescription: "Indicates if branches are automatically deleted when pull requests are merged.",
+				Computed:            true,
+			},
+			"use_squash_pr_title_as_default": schema.BoolAttribute{
+				Description:         "Indicates if the squash PR title is used as the default commit message.",
+				MarkdownDescription: "Indicates if the squash PR title is used as the default commit message.",
+				Computed:            true,
+			},
+			"squash_merge_commit_title": schema.StringAttribute{
+				Description:         "The title of squash merge commits for pull requests.",
+				MarkdownDescription: "The title of squash merge commits for pull requests.",
+				Computed:            true,
+			},
+			"squash_merge_commit_message": schema.StringAttribute{
+				Description:         "The message of squash merge commits for pull requests.",
+				MarkdownDescription: "The message of squash merge commits for pull requests.",
+				Computed:            true,
+			},
+			"merge_commit_title": schema.StringAttribute{
+				Description:         "The title of merge commits for pull requests.",
+				MarkdownDescription: "The title of merge commits for pull requests.",
+				Computed:            true,
+			},
+			"merge_commit_message": schema.StringAttribute{
+				Description:         "The message of merge commits for pull requests.",
+				MarkdownDescription: "The message of merge commits for pull requests.",
+				Computed:            true,
+			},
+			"archived": schema.BoolAttribute{
+				Description:         "Indicates if the repository is archived.",
+				MarkdownDescription: "Indicates if the repository is archived.",
+				Computed:            true,
+			},
+			"disabled": schema.BoolAttribute{
+				Description:         "Indicates if the repository is disabled.",
+				MarkdownDescription: "Indicates if the repository is disabled.",
+				Computed:            true,
+			},
 		},
 		Description:         "Use this data source to retrieve a list of GitHub repositories.",
 		MarkdownDescription: "Use this data source to retrieve a list of GitHub repositories.",
@@ -67,7 +277,6 @@ func (d *GitHubRepository) Schema(_ context.Context, _ datasource.SchemaRequest,
 }
 
 func (d *GitHubRepository) Configure(_ context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
-	// ProviderData is nil until the ConfigureProvider RPC is called.
 	if req.ProviderData == nil {
 		return
 	}
@@ -115,8 +324,43 @@ func (d *GitHubRepository) Read(ctx context.Context, req datasource.ReadRequest,
 	}
 
 	model.Id = types.Int64Value(repo.GetID())
+	model.NodeID = types.StringValue(repo.GetNodeID())
 	model.Name = types.StringValue(repo.GetName())
 	model.FullName = types.StringValue(repo.GetFullName())
+	model.Description = types.StringValue(repo.GetDescription())
+	model.Homepage = types.StringValue(repo.GetHomepage())
+	model.DefaultBranch = types.StringValue(repo.GetDefaultBranch())
+	model.MasterBranch = types.StringValue(repo.GetMasterBranch())
+	model.HTMLURL = types.StringValue(repo.GetHTMLURL())
+	model.CloneURL = types.StringValue(repo.GetCloneURL())
+	model.GitURL = types.StringValue(repo.GetGitURL())
+	model.MirrorURL = types.StringValue(repo.GetMirrorURL())
+	model.SSHURL = types.StringValue(repo.GetSSHURL())
+	model.SVNURL = types.StringValue(repo.GetSVNURL())
+	model.Language = types.StringValue(repo.GetLanguage())
+	model.Fork = types.BoolValue(repo.GetFork())
+	model.ForksCount = types.Int64Value(int64(repo.GetForksCount()))
+	model.NetworkCount = types.Int64Value(int64(repo.GetNetworkCount()))
+	model.OpenIssuesCount = types.Int64Value(int64(repo.GetOpenIssuesCount()))
+	model.StargazersCount = types.Int64Value(int64(repo.GetStargazersCount()))
+	model.SubscribersCount = types.Int64Value(int64(repo.GetSubscribersCount()))
+	model.Size = types.Int64Value(int64(repo.GetSize()))
+	model.AutoInit = types.BoolValue(repo.GetAutoInit())
+	model.AllowRebaseMerge = types.BoolValue(repo.GetAllowRebaseMerge())
+	model.AllowUpdateBranch = types.BoolValue(repo.GetAllowUpdateBranch())
+	model.AllowSquashMerge = types.BoolValue(repo.GetAllowSquashMerge())
+	model.AllowMergeCommit = types.BoolValue(repo.GetAllowMergeCommit())
+	model.AllowAutoMerge = types.BoolValue(repo.GetAllowAutoMerge())
+	model.AllowForking = types.BoolValue(repo.GetAllowForking())
+	model.WebCommitSignoffRequired = types.BoolValue(repo.GetWebCommitSignoffRequired())
+	model.DeleteBranchOnMerge = types.BoolValue(repo.GetDeleteBranchOnMerge())
+	model.UseSquashPRTitleAsDefault = types.BoolValue(repo.GetUseSquashPRTitleAsDefault())
+	model.SquashMergeCommitTitle = types.StringValue(repo.GetSquashMergeCommitTitle())
+	model.SquashMergeCommitMessage = types.StringValue(repo.GetSquashMergeCommitMessage())
+	model.MergeCommitTitle = types.StringValue(repo.GetMergeCommitTitle())
+	model.MergeCommitMessage = types.StringValue(repo.GetMergeCommitMessage())
+	model.Archived = types.BoolValue(repo.GetArchived())
+	model.Disabled = types.BoolValue(repo.GetDisabled())
 
 	resp.State.Set(ctx, &model)
 }
