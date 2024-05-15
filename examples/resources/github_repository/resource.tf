95cefddaed9e6cacf39ade9b5f79ce4719403b1f
Craig Sloggett <craigsloggett@gmail.com>
2024-05-15 11:43:11 -0400

    docs: Update docs and regenerate (#19)

--- examples/resources/github_repository/resource.tf
+++ examples/resources/github_repository/resource.tf
@@ -1,23 +1,24 @@
 resource "github_repository" "example" {
-  name                        = "testing-repository"
+  name = "testing-repository"
+
+  allow_auto_merge            = true
+  allow_merge_commit          = true
+  allow_rebase_merge          = false
+  allow_squash_merge          = true
+  auto_init                   = true
+  delete_branch_on_merge      = true
   description                 = "This is a description."
-  homepage                    = "https://github.com"
-  private                     = true
+  gitignore_template          = "Terraform"
+  has_discussions             = true
   has_issues                  = true
   has_projects                = true
   has_wiki                    = true
-  has_discussions             = true
-  auto_init                   = true
-  gitignore_template          = "Terraform"
-  license_template            = "mpl-2.0"
-  allow_squash_merge          = true
-  allow_merge_commit          = true
-  allow_rebase_merge          = false
-  allow_auto_merge            = true
-  delete_branch_on_merge      = true
-  squash_merge_commit_title   = "PR_TITLE"
-  squash_merge_commit_message = "COMMIT_MESSAGES"
-  merge_commit_title          = "PR_TITLE"
-  merge_commit_message        = "PR_BODY"
+  homepage                    = "https://github.com"
   is_template                 = true
+  license_template            = "mpl-2.0"
+  merge_commit_message        = "PR_BODY"
+  merge_commit_title          = "PR_TITLE"
+  private                     = true
+  squash_merge_commit_message = "COMMIT_MESSAGES"
+  squash_merge_commit_title   = "PR_TITLE"
 }
